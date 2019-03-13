a143d8efca704da942aa6da039e5568c30e0e505
Kashan Siddiqui <kashanzahoorsiddiqui@gmail.com>
2019-03-13 14:32:47 +0500

    home page contact fix

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OutlinedTextFields from './shared/textfield'
 import ContainedButtons from './shared/button'
-
+import '../asssets/css/homeContact.css'
 
 class Counter extends Component {
   render() {
@@ -11,18 +11,19 @@ class Counter extends Component {
         <section className="stats" id="plans" >
         <div style={{textAlign:'center',color:'white'}}><h3>Ready to get your workplace started?</h3>
         <h2>Lets Talk</h2> </div>
-        <div style={{width:'300px',margin:'0 auto'}}>
+        <div className='mainDiv'>
         <div style={{display:'inline-flex'}}>
-        <OutlinedTextFields text='Full Name' require/>
-        <OutlinedTextFields text='Phone Number' require/>
+        {/* <OutlinedTextFields text='Full Name' require/>
+        <OutlinedTextFields text='Phone Number' require/> */}
+        <input type='text' className='customInputHalf ' placeholder="Full Name"/>
+        <input type='text' className='customInputHalf' placeholder="Phone Number"/>
         </div>
-         {/* <div style={{display:'flex'}}>
-        <OutlinedTextFields text='Email'/>
-        <OutlinedTextFields text='Phone Number'/>
-        </div>  */}
+                 
         <div >
-        <OutlinedTextFields text='Email' require/>
-        <OutlinedTextFields text='brief description' />
+      {/* <OutlinedTextFields text='Email'  require/>
+        <OutlinedTextFields text='brief description' /> */}
+          <input type='text' placeHolder='Email' className='customInputFull'/>
+          <input type='text' placeHolder='Brief Description' className='customInputFull'/>
         </div>
         <div style={{marginTop:'25px',textAlign:'center'}}>
           <ContainedButtons text='Submit' color='#96684F' margin='10px' width='150px'/>
