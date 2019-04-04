caddbcde9bbc0556a076d4c6517fae1ff7eaefea
Kashan Siddiqui <kashanzahoorsiddiqui@gmail.com>
2019-04-04 11:41:58 +0500

    random

---
+++ src/redux/reducers/projects_reducer.js
@@ -0,0 +1,17 @@
+import { EVENT_DETAILS } from "../actions/event-actions";
+import { EDIT_OBJ } from "../actions/root.action";
+
+const INITIAL_STATE = {
+edit_obj : {}
+}
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case EDIT_OBJ : 
+        return({
+            edit_obj : action.payload
+        })
+        default: 
+        return state
+    }
+}
